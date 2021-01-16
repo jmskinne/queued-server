0d959077e84826c67f81c02854229c958ffbdc81
Mark Skinner <jmskinne@gmail.com>
2021-01-16 15:08:36 -0600

    cors

--- queued/settings.py
+++ queued/settings.py
@@ -68,7 +68,9 @@ INSTALLED_APPS = [
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://queued-client-krnqm.ondigitalocean.app/'
+
 )
 
 MIDDLEWARE = [
